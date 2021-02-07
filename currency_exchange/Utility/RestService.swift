8c23a18cf04da20afd757374f19494a0cc1880b7
Alexander Berezovsky <a.a.berezovsky@gmail.com>
2021-02-07 19:01:32 +0200

    original commit

---
+++ currency_exchange/Utility/RestService.swift
@@ -0,0 +1,17 @@
+//
+//  RestService.swift
+//  currency_exchange
+//
+//  Created by atomic on 07.02.2021.
+//
+
+import Foundation
+
+final class RestService {
+    
+    static let shared = RestService()
+    
+    private init() {
+        
+    }
+}
--- currency_exchange/ViewController.swift
+++ currency_exchange/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emptyView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    fileprivate func addCurrency() {
+        
+    }
 
 }
 
